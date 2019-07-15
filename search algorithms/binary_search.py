45fe301672351729a66409e74faa79d90dcc37db
Халион <xalion67@gmail.com>
2019-07-16 02:45:21 +0300

    delete 'example' code

--- search algorithms/binary_search.py
+++ search algorithms/binary_search.py
@@ -1,4 +1,6 @@
 # The function searches the index of an element in an array.
+
+
 def binary_search(item, array):
     low = 0
     high = len(array) - 1
@@ -14,13 +16,3 @@ def binary_search(item, array):
             high = mid - 1
 
     return None
-
-
-# Work example
-sorted_array = [1, 3, 5, 7, 9, 11, 13, 15]
-element_find = binary_search(7, sorted_array)
-element_not_find = binary_search(4, sorted_array)
-
-print('Sorted array >> ' + str(sorted_array))
-print('Index number 7 in array = ' + str(element_find))
-print('Index number 4 in array = ' + str(element_not_find))
