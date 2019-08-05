8e18a8d98fc5b42af78b923e9f54b2061bd853eb
Халион <xalion67@gmail.com>
2019-08-05 03:52:07 +0300

    rename variable

--- writer.py
+++ writer.py
@@ -3,13 +3,13 @@ from time import sleep
 import sys
 
 
-GREEN = '\033[38;5;48m'
+START_GREEN = '\033[38;5;48m'
 END_GREEN = '\033[0m'
 
 
 while True:
     try:
-        print(GREEN + chr(randint(33, 126)) + END_GREEN, end='')
+        print(START_GREEN + chr(randint(33, 126)) + END_GREEN, end='')
         sys.stdout.flush()
         sleep(0.005)
     except KeyboardInterrupt:
